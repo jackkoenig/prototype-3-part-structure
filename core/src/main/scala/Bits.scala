307eecd4d70b9e6b6b3de156d7afbf280dd16bff
Jack Koenig <koenig@sifive.com>
2024-09-11 16:02:34 -0700

    Split class UInt into its own file

--- core/src/main/scala/Bits.scala
+++ core/src/main/scala/Bits.scala
@@ -32,32 +32,6 @@ abstract class Bits(val width: Option[Int]) {
   }
 }
 
-class UInt(width: Option[Int]) extends Bits(width) {
-  final def +(n: UInt): UInt = macro SourceInfoTransform.nArg
-
-  def do_+(n: UInt)(implicit info: FakeSourceInfo): UInt = {
-    val w = (this.width, n.width) match {
-      case (None, _) => None
-      case (_, None) => None
-      case (Some(x), Some(y)) => Some(x + y)
-    }
-    println(s"[$info] Calling + on $this with $n gives resulting width $w")
-    new UInt(w)
-  }
-
-  final def -(n: UInt): UInt = macro SourceInfoTransform.nArg
-
-  def do_-(n: UInt)(implicit info: FakeSourceInfo): UInt = {
-    val w = (this.width, n.width) match {
-      case (None, _) => None
-      case (_, None) => None
-      case (Some(x), Some(y)) => Some(x - y)
-    }
-    println(s"[$info] Calling - on $this with $n gives resulting width $w")
-    new UInt(w)
-  }
-}
-
 // Normally this would be in a different file, but its here to padd up the size of the
 // cross-compilable version of this
 object Main {
---
+++ core/src/main/scala/UInt.scala
@@ -0,0 +1,30 @@
+
+package fakechisel
+
+import fakechisel.macros.SourceInfoTransform
+
+class UInt(width: Option[Int]) extends Bits(width) {
+  final def +(n: UInt): UInt = macro SourceInfoTransform.nArg
+
+  def do_+(n: UInt)(implicit info: FakeSourceInfo): UInt = {
+    val w = (this.width, n.width) match {
+      case (None, _) => None
+      case (_, None) => None
+      case (Some(x), Some(y)) => Some(x + y)
+    }
+    println(s"[$info] Calling + on $this with $n gives resulting width $w")
+    new UInt(w)
+  }
+
+  final def -(n: UInt): UInt = macro SourceInfoTransform.nArg
+
+  def do_-(n: UInt)(implicit info: FakeSourceInfo): UInt = {
+    val w = (this.width, n.width) match {
+      case (None, _) => None
+      case (_, None) => None
+      case (Some(x), Some(y)) => Some(x - y)
+    }
+    println(s"[$info] Calling - on $this with $n gives resulting width $w")
+    new UInt(w)
+  }
+}
