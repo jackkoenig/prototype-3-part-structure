7f8303a477122b619e554b0b6e3134ad79330896
Jack Koenig <koenig@sifive.com>
2024-09-11 16:31:45 -0700

    Move Main from scala-2 to scala
    Both Scala 2 and 3 now fully work.

--- core/src/main/scala-2/Bits.scala
+++ core/src/main/scala-2/Bits.scala
@@ -19,27 +19,3 @@ abstract class Bits(val width: Option[Int]) extends BitsImpl {
 
   def do_apply(n: Int)(implicit info: FakeSourceInfo): UInt = _applyImpl(n)
 }
-
-// Normally this would be in a different file, but its here to padd up the size of the
-// cross-compilable version of this
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello World!")
-    val x = new UInt(Some(3))
-    println(x)
-    println(x.tail(3))
-    val y = new UInt(None)
-    println(y)
-    println(y.head(4))
-
-    val z = x + y
-    println(z)
-
-    val a = new UInt(Some(2))
-    val b = x - a
-    println(b)
-
-    val c = x(2)(0)
-    println(c)
-  }
-}
